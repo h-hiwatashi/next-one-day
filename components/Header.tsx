39f234c7ab7cb86ce9d6bf3196d0c6968188f56c
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-04-20 21:25:16 +0900

    init

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="flex flex-wrap gap-[24px] h-[80px] items-center">
+        <div>
+          <h2 className="text-[3rem]">0120-xxx-xxx</h2>
+        </div>
+        <div>Button</div>
+        <div>Button</div>
+        <div>Button</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
